7df5f45fabfaafa45a19e1eab06cd2ee17440a7d
zhihaofang1017 <92305118+zhihaofang1017@users.noreply.github.com>
2021-11-06 23:40:10 +0000

    leecode85

---
+++ hard_question/index85(最大矩阵).py
@@ -0,0 +1,40 @@
+def maximalRectangle(matrix):
+    """
+    :type matrix: List[List[str]]
+    :rtype: int
+    """
+    List = []
+    for i in range(len(matrix)):
+        temp = []
+        count = 0
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "1":
+                count = count + 1
+                temp.append(count)
+            else:
+                count = 0
+                temp.append(count)
+        List.append(temp)
+    print(List)
+    ans = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            height = 0
+            for k in range(i, len(matrix)):
+                if List[i][j] <= List[k][j] and List[i][j] != 0:
+                    height = height + 1
+                else:
+                    break
+            for k in range(i-1, -1, -1):
+                if List[i][j] <= List[k][j]and List[i][j] != 0:
+                    height = height + 1
+                else:
+                    break
+            # print(str(i)+","+str(j)+"  "+str(height))
+            ans = max(ans,height*List[i][j])
+    print(ans)
+
+
+
+
+maximalRectangle([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
