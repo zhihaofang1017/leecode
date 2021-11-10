e7bbcb95df112f04e987516b2039f81f347ded70
zhihaofang1017 <92305118+zhihaofang1017@users.noreply.github.com>
2021-11-10 17:14:26 +0000

    finish Leecode213

--- DP_question/leecode213.py
+++ DP_question/leecode213.py
@@ -1,54 +1,53 @@
 def rob(nums):
-    if len(nums) <= 3:
-        return max(nums)
+    if len(nums)==1:
+        return nums[0]
+    if len(nums)==2:
+        return max(nums[0],nums[1])
     nums1 = nums[0:-1]
     nums2 = nums[1:]
 
     temp1 = nums1[0]
     temp2 = nums1[1]
+    temp5 = nums1[1]
+    hashtable1 = dict()
+    hashtable1[nums1[0]] = 0
 
-    for i in range(3, len(nums1)):
-        if i % 2 == 1:
+    for i in range(2,len(nums1)):
+        if i%2 == 0:
+            temp1 = max(max(hashtable1),temp1)
             temp1 = temp1 + nums1[i]
+            hashtable1[temp5] = i-1
+            temp5 = temp1
         else:
+            temp2 = max(max(hashtable1), temp2)
             temp2 = temp2 + nums1[i]
+            hashtable1[temp5] = i-1
+            temp5 = temp2
 
     temp3 = nums2[0]
     temp4 = nums2[1]
-    for i in range(3, len(nums2)):
-        if i % 2 == 1:
+    hashtable2 = dict()
+    hashtable2[nums2[0]] = 0
+    temp6 = nums2[1]
+    for i in range(2,len(nums2)):
+        if i%2 == 0:
+            print(hashtable2)
+            temp3 = max(max(hashtable2), temp3)
             temp3 = temp3 + nums2[i]
+            hashtable2[temp6] = i-1
+            temp6 = temp3
         else:
+            temp4 = max(max(hashtable2), temp4)
             temp4 = temp4 + nums2[i]
-
-    temp5 = nums1[0]
-    temp6 = nums1[1]
-
-    for i in range(2, len(nums1)):
-        if i % 2 == 0:
-            temp5 = temp5 + nums1[i]
-        else:
-            temp6 = temp6 + nums1[i]
-
-    temp7 = nums2[0]
-    temp8 = nums2[1]
-    for i in range(2, len(nums2)):
-        if i % 2 == 0:
-            temp7 = temp7 + nums2[i]
-        else:
-            temp8 = temp8 + nums2[i]
+            hashtable2[temp6] = i - 1
+            temp6 = temp4
     print(temp1)
     print(temp2)
     print(temp3)
     print(temp4)
-    print(temp5)
-    print(temp6)
-    print(temp7)
-    print(temp8)
-    ans = max(temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8)
+    ans = max(temp1,temp2,temp3,temp4)
+    print(ans)
     return ans
 
-
-# rob([1, 2, 3, 1])
-rob([6, 3, 10, 8, 2, 10, 3, 5, 10, 5, 3])
-# rob([1, 3, 1, 3, 100])
+rob([6,6,4,8,4,3,3,10])
+rob([1,3,1,3,100])
